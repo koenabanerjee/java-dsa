c01433226187e3469add34d36281748065a60897
Koena Banerjee <koenabanerjee966@gmail.com>
2026-05-26 11:32:14 +0530

    new problems

---
+++ Arrays/SortZeroOneTwo.java
@@ -0,0 +1,52 @@
+package Arrays;
+import java.util.*;
+
+
+class SortZeroOneTwo{
+    //THIS IS THE THE OPTIMAL APPROACH WHICH USES THE DUTCH NATIONAL FLAG ALGORITHM
+    // THE BETTER APPROACH USES THREE COUNTER VARIABLES TO COUNTER THE FREQUENCIES Of 1s, 2s and 3s AND THEN OVERWRITE THEM INTO THE ARRAY
+    public void DutchNationalFlagAlgo(int[] nums){
+        int n = nums.length;
+        int low = 0;
+        int mid = 0;
+        int high = n - 1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                //swap element with element at low
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                //we will move the mid pointer forward
+                mid++;
+            }
+            else{
+                //if nums[mid] == 2
+                //swap element with nums[high]
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high --;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 0 , 1, 2, 1, 2, 0, 0, 0, 1};
+        SortZeroOneTwo sol = new SortZeroOneTwo();
+        sol.DutchNationalFlagAlgo(nums);
+
+        for(int num : nums){
+            System.out.print(num + " ");
+        }
+
+        
+
+    }
+}
---
+++ Arrays/majorityElement.java
@@ -0,0 +1,48 @@
+package Arrays;
+
+public class majorityElement {
+
+    //OPTIMAL APPROACH -> MOORE'S VOTING ALGORITHM
+    public static int majorityEle(int[] arr){
+        int n = arr.length;
+        int cnt = 0;
+        int el = 0;
+
+        //finding the potential majority element
+        for(int i = 0;i<n;i++){
+            if(cnt == 0){
+                cnt = 1;
+                el = arr[i];
+            }
+
+            else if(arr[i] == el){
+                cnt++;
+            } else{
+                cnt--;
+            }
+        }
+
+        //counting the freq of potential majority element
+        int cnt1 = 0;
+        for(int i = 0;i<n;i++){
+            if(arr[i] == el){
+                cnt1++;
+            }
+        }
+
+        //Checking if the potential majority element is actually the majority element in the array - if it is greater than n/2
+        if(cnt1 > n/2){
+            return el;
+        }
+
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7, 7, 5, 7, 5, 1, 5, 7, 5, 5, 7, 7, 5, 5, 5, 5};
+        majorityElement sol = new majorityElement();
+        int result = sol.majorityEle(arr);
+        System.out.println("The majority element in the array is "+ result);
+    }
+}
